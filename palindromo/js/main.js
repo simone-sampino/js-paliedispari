7f838fd49b38c8eda9a874d9971dd6ef81e53da9
simone-sampino <s.samp98@gmail.com>
2025-06-04 09:08:15 +0200

    Refactor palindrome function to use userWord directly and improve readability

--- palindromo/js/main.js
+++ palindromo/js/main.js
@@ -3,14 +3,24 @@ let userWord = prompt("Inserisci una parola");
 console.log(userWord);
 
 // Creare una funzione per capire se la parola inserita è palindroma
-function palindrome(word) {
-  let palindrome = "";
+function palindrome() {
+  let thisWord = "";
 
-  for (let i = word.length - 1; i >= 0; i--) {
-    const reverseWord = word.charAt(i);
-    palindrome = palindrome + reverseWord;
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    const reverseWord = userWord.charAt(i);
+    thisWord = thisWord + reverseWord;
   }
-  return palindrome;
+  return thisWord;
 }
 
-console.log(palindrome(userWord));
+let isPalindrome = palindrome();
+
+function reversed(userWord) {
+  if (userWord === isPalindrome) {
+    console.log("La parola inserita è palindroma: " + isPalindrome);
+  } else {
+    console.log("La parola inserita non è palindroma");
+  }
+}
+
+reversed(userWord);
